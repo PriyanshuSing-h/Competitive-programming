cab94f77d81f7c66e2e45617e97d32571819ba49
Priyanshu Singh <priyanshuthakur6394@gmail.com>
2025-08-19 14:45:52 +0530

    Updated

--- domino.java
+++ domino.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 public class domino {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int m=sc.nextInt();
-        int n=sc.nextInt();
-        System.out.println((m*n)/2);
+        int row=sc.nextInt();
+        int col=sc.nextInt();
+        System.out.println((row*col)/2);
         sc.close();
     }
 }
